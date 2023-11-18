5bd36b2f498296ecdae0c5a7cd02159ea8ec3f36
Wilsonabdiel <wilsonabdiel86@gmail.com>
2023-11-18 06:48:04 +0100

    Feat:404.html

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,14 +1,31 @@
 from django.shortcuts import render
+
 from markdown2 import Markdown
+
 from . import util
 
-def covnvert_md_html(title):
+def convert_md_html(title):
     content = util.get_entry(title)
-    markdower = Markdown()
+    markdowner = Markdown()
+    if content == None:
+        return None
+    else:
+        return markdowner.convert(content)
+
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
 def entry(request,  title):
-    return
+    content = convert_md_html(title)
+    if content == None:
+        return render(request, "encyclopedia/404.html", {
+            "message": "Entry Not Found",
+            "callToAction": "Create one"
+        })
+    else:
+        return render(request, "encyclopedia/entry.html", {
+            "title": title,
+            "content": content
+        })
