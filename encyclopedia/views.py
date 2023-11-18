27b97e5ad83ca610b37e00458da0b5b047195698
Wilsonabdiel <wilsonabdiel86@gmail.com>
2023-11-18 06:13:19 +0100

    bugs

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
-
+from markdown2 import Markdown
 from . import util
 
-
+def covnvert_md_html(title):
+    content = util.get_entry(title)
+    markdower = Markdown()
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
-def entry(request, name):
-    return render(request, "encyclopedia/entry.html")
-
+def entry(request,  title):
+    return
