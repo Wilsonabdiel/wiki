7a2b47a1870abd0c63e38874d2e4dec41d8a2712
Wilsonabdiel <wilsonabdiel86@gmail.com>
2023-11-19 11:52:04 +0100

    git wip

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -5,8 +5,8 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:title>", views.entry, name="entry"),
-    path("search/", views.search, name="search"),
-    # path("new/", views.new_page, name="new_page"),
+    # path("search/", views.search, name="search"),
+    path("new/", views.new_page, name="new_page"),
     # path("edit/", views.edit, name="edit"),
     # path("saveEdit/", views.saveEdit, name="saveEdit"),
     # path("rando/", views.rando, name="rando"),
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -42,39 +42,72 @@ def entry(request, title):
         })
 
 
-def search(request):
-    entry_search = request.GET.get('q', '').strip()
-
-    if util.get_entry(entry_search) is not None:
-        content = convert_md_to_html(entry_search)
-        return render(request, "encyclopedia/entry.html", {
-            "title": entry_search,
-            "content": content,
-        })
-    else:
-        recommendation = [entry for entry in util.list_entries() if entry_search.lower() in entry.lower()]
-        return render(request, "encyclopedia/search.html", {"recommendation": recommendation})
-
-
+# def search(request):
+#     all_entries = util.list_entries()
 #
-# def new_page(request):
 #     if request.method == "GET":
-#         return render(request, "encyclopedia/new.html")
-#     else:
-#         title = request.POST['title']
-#         content = request.POST['content']
-#         titleExist = util.get_entry(title)
-#         if titleExist is not None:
-#             return render(request, "encyclopedia/error.html", {"message": "Entry already exist"})
-#         else:
-#             util.save_entry(title, content)
-#             html_content = convert_md_to_html(title)
-#             return render(request, "encyclopedia/entry.html", {
-#                 "title": title,
-#                 "content": html_content
-#             })
+#         entry_search = request.GET.get('q', '').strip()
+#         html_content = convert_md_to_html(entry_search)
 #
-#         # return render(request, "encyclopedia)
+#         if html_content is not None:
+#             # If content is found, redirect to the entry page
+#             return redirect('entry', title=entry_search.strip("?q="))  # Adjust 'entry_page' to your actual entry URL pattern name
+#         else:
+#             # If content is not found, search for recommendations
+#             recommendations = [entry for entry in all_entries if entry_search.lower() in entry.lower()]
+#
+#             if recommendations:
+#                 # If recommendations are found, render the search results page
+#                 return render(request, "encyclopedia/search.html", {
+#                     "recommendations": recommendations
+#                 })
+#             else:
+#                 # If no matching entries or recommendations found, show an error
+#                 return render(request, "encyclopedia/404.html", {
+#                     "message": "No such entry exists."
+#                 })
+#
+#     else:
+#         return render(request, "encyclopedia/404.html", {
+#             "message": "Invalid request method."
+#         })
+
+#
+# def search(request):
+#     entry_search = request.GET.get('q', '').strip()
+#
+#     if util.get_entry(entry_search) is not None:
+#         content = convert_md_to_html(entry_search)
+#         return render(request, "encyclopedia/entry.html", {
+#             "title": entry_search,
+#             "content": content,
+#         })
+#     else:
+#         recommendation = [entry for entry in util.list_entries() if entry_search.lower() in entry.lower()]
+#         return render(request, "encyclopedia/search.html", {
+#             "recommendation": recommendation
+#         })
+
+
+
+def new_page(request):
+    if request.method == "GET":
+        return render(request, "encyclopedia/new.html")
+    else:
+        title = request.POST['title']
+        content = request.POST['content']
+        titleExist = util.get_entry(title)
+        if titleExist is not None:
+            return render(request, "encyclopedia/404.html", {"message": "Entry already exist"})
+        else:
+            util.save_entry(title, content)
+            html_content = convert_md_to_html(title)
+            return render(request, "encyclopedia/entry.html", {
+                "title": title,
+                "content": html_content
+            })
+
+        # return render(request, "encyclopedia)
 #
 #
 # def edit(request):
